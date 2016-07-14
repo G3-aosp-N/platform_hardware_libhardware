a7a98ed5fd08c6d2aae5bcab6b5f2446a594c6f1
Chenjie Luo <cjluo@google.com>
2016-07-14 15:46:58 -0700

    power: Adds POWER_HINT_DISABLE_TOUCH
    POWER_HINT_DISABLE_TOUCH is used to hint power hal that device is
    in a state that touch controller could be disabled to save power.
    
    Bug: 30143923
    Change-Id: I56c588e62afadffdb367b6e2c3115cffef51bb45
    (cherry picked from commit 90db35122df088212dfef1c09af9d5470363d6c0)

--- include/hardware/power.h
+++ include/hardware/power.h
@@ -67,9 +67,11 @@ typedef enum {
     POWER_HINT_CAM_PREVIEW = 0x00000008,
 
     POWER_HINT_CPU_BOOST    = 0x00000010,
-    POWER_HINT_LAUNCH_BOOST = 0x00000011,
     POWER_HINT_AUDIO        = 0x00000020,
     POWER_HINT_SET_PROFILE  = 0x00000030
+
+    POWER_HINT_LAUNCH = 0x00000008,
+    POWER_HINT_DISABLE_TOUCH = 0x00000009
 
 } power_hint_t;
 
@@ -264,11 +266,13 @@ typedef struct power_module {
      *     device can sustain it. The data parameter is non-zero when the mode
      *     is activated and zero when deactivated.
      *
-     * POWER_HINT_CPU_BOOST
+     * POWER_HINT_DISABLE_TOUCH
      *
-     *     An operation is happening where it would be ideal for the CPU to
-     *     be boosted for a specific duration. The data parameter is an
-     *     integer value of the boost duration in microseconds.
+     *     When device enters some special modes, e.g. theater mode in Android
+     *     Wear, there is no touch interaction expected between device and user.
+     *     Touch controller could be disabled in those modes to save power.
+     *     The data parameter is non-zero when touch could be disabled, and zero
+     *     when touch needs to be re-enabled.
      *
      * A particular platform may choose to ignore any hint.
      *
